476a0f35e6f4f2f3f5ed71127518b5e58a6f8997
locluyencode <81458734+locluyencode@users.noreply.github.com>
2023-03-12 15:27:29 +0700

    update study

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import logo from "./logo.svg";
 import gif from "./gif2.gif";
 import "./App.css";
-import TodoFuture from "./features/Todo";
-import AlbumFuture from "./features/Album";
-import Counter from "./components/Counter";
-import ColorBox from "./components/ColorBox";
-import { Routes, Route, Link } from "react-router-dom";
-import NotFound from "./components/NotFound";
 
 function App() {
   const name = "Cậu Tốm";
@@ -17,12 +10,6 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <Link to={"/"}>home</Link>
-        <Link to={"/todo-list"}>todo-list</Link>
-        <Link to={"/album-future"}>album-future</Link>
-        <Link to={"/counter"}>counter</Link>
-        <Link to={"/color-box"}>color-box</Link> */}
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <img src={gif} className="App-logo" alt="logo" />
         <p>{name}</p>
         {isMale && <p>Male</p>}
@@ -40,18 +27,6 @@ function App() {
         <a className="App-link" href={aws}>
           <li>AWS</li>{" "}
         </a>
-
-        {/* <TodoFuture /> */}
-        {/* <AlbumFuture /> */}
-        {/* <Counter /> */}
-        {/* <ColorBox /> */}
-        {/* <Routes>
-          <Route path="/todo-list" exact element={<TodoFuture />} />
-          <Route path="/album-future" element={<AlbumFuture />} />
-          <Route path="/counter" element={<Counter />} />
-          <Route path="/color-box" element={<ColorBox />} />
-          <Route element={<NotFound />} />
-        </Routes> */}
       </header>
     </div>
   );
