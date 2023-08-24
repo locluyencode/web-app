c8659b970b71cd542e4aff9584675a9d5e3da617
locluyencode <81458734+locluyencode@users.noreply.github.com>
2023-08-24 22:22:10 +0700

    add android

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ function App() {
   const fb = "https://facebook.com/baloc.nguyen14";
   const aws = "https://graduation.udacity.com/confirm/EPKHAJHT";
   const java = "https://graduation.udacity.com/confirm/RZV6WJTU";
+  const android =
+    "https://graduation.udacity.com/confirm/e/facf122c-fb5c-11ed-8e86-07065207c643";
   return (
     <div className="App">
       <header className="App-header">
@@ -25,12 +27,17 @@ function App() {
           My FB
         </a>
         <p>Study</p>
-        <a className="App-link" href={aws}>
-          <li>AWS</li>{" "}
-        </a>
-        <a className="App-link" href={java}>
-          <li>JAVA</li>{" "}
-        </a>
+        <ul>
+          <a className="App-link" href={aws}>
+            <li>AWS</li>{" "}
+          </a>
+          <a className="App-link" href={java}>
+            <li>JAVA</li>{" "}
+          </a>
+          <a className="App-link" href={android}>
+            <li>ANDROID</li>{" "}
+          </a>
+        </ul>
       </header>
     </div>
   );
